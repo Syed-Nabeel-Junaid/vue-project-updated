25b09ea5a04963ace608a5e2255b8cb8807b7fbb
Nabeel Junaid <snabeelj@gmail.com>
2023-03-27 17:23:17 +0400

    first edit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+   <HomeW  />
+
+</div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import HomeW from './components/HomeW.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    HomeW
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="child">
+    <h1>products</h1>
+ 
+  </div>
+</template>
+<script>
+export default {
+  name: "MyProducts",
+
+};
+</script>
+<style scoped>
+.child {
+  background-color: rgb(50, 184, 205);
+  height: 150px;
+  width: 200px;
+}
+</style>
