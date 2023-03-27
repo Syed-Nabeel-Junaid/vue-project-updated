e9b83348a32496b87b5d1cdbac5f5e3d3cee0ae1
Nabeel Junaid <snabeelj@gmail.com>
2023-03-27 15:30:41 +0400

    second commit

---
+++ src/components/HomeW.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home">
+    <h1>loop</h1>
+   
+   
+
+    <MyUsers />
+    <MyProducts />
+  </div>
+</template>
+
+<script>
+import MyUsers from './Users.vue'
+
+import MyProducts from './Products.vue'
+
+export default {
+  name: "HomeW",
+  components: {
+    MyUsers,
+    MyProducts,
+  },
+  props: {
+    data: String,
+    msg: String,
+  },
+  methods: {
+    hello() {
+      this.show = !this.show;
+    },
+  },
+  data() {
+    return {
+      users: [
+        { id: 1, name: "nab", email: "nab@test.com" },
+        { id: 2, name: "aab", email: "aab@test.com" },
+        { id: 3, name: "bab", email: "bab@test.com" },
+        { id: 4, name: "cab", email: "cab@test.com" },
+        { id: 5, name: "dab", email: "dab@test.com" },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.home {
+  background-color: yellowgreen;
+  height: 500px;
+}
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="child">
+    <h1>users</h1>
+    
+    <!-- <h1>{{data}}</h1>
+    <h1>{{msg}}</h1> -->
+  </div>
+</template>
+<script>
+export default {
+  name: "MyUsers",
+  props: {
+    data: String,
+    msg: String,
+  },
+};
+</script>
+<style scoped>
+.child {
+  background-color: rgb(50, 184, 205);
+  height: 150px;
+  width: 200px;
+}
+</style>
